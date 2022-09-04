24179bb250de89c02ee2b2c1246d29f70f2f26b5
Artiver <l952011340@qq.com>
2022-09-04 12:02:56 +0800

    add annotation

--- src/main/java/org/artiver/myspringboot/pojo/User.java
+++ src/main/java/org/artiver/myspringboot/pojo/User.java
@@ -1,9 +1,18 @@
 package org.artiver.myspringboot.pojo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
 // 使用该注解等于实现了pojo类的tostring getter setter hash equals等方法
+@NoArgsConstructor
+// 自动生成无参构造函数
+@AllArgsConstructor
+// 自动生成所有参数的构造函数
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+// 返回json响应时自动去除null的字段
 public class User {
     int id;
     String username;
